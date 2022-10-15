faa4a3853366022bcbefca3a8b9caa1bae6b6c57
Codeyg12 <codey.gallup@gmail.com>
2022-10-15 16:43:27 -0600

    cards completed

--- src/components/pages/Card.js
+++ src/components/pages/Card.js
@@ -1,1 +1,52 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+
+const styles = {
+    border: {
+        width: '90%',
+    },
+    img: {
+        width: '85%',
+        height: '85%',
+    },
+    title: {
+        fontSize: '1.2em',
+        fontWeight: 'bold'
+    },
+    icon: {
+        fontSize: '2em',
+        margin: '0 5%',
+        color: 'black'
+    }
+}
+
+export default function Card(props) {
+    function showDetails(e) {
+        console.log(e)
+        e.target.style.width = '100%'
+        e.target.style.height = '100%'
+    }
+    
+    function hideDetails(e) {
+        e.target.style.width = '85%'
+        e.target.style.height = '85%'
+    }
+
+  return (
+    <div style={styles.border}>
+      <div>
+        <h2>{props.name}</h2>
+      </div>
+      <div>
+        <figcaption style={styles.title}>{props.title}</figcaption>
+        <img style={styles.img} src={props.image} onMouseEnter={showDetails} onMouseLeave={hideDetails}></img>
+      </div>
+      <div>
+        <a style={styles.icon} href={props.github}><FontAwesomeIcon icon={faGithub} /></a>
+        <a style={styles.icon} href={props.link}><FontAwesomeIcon icon={faLink} /></a>
+      </div>
+    </div>
+  );
+}
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,16 +1,36 @@
 import React from "react";
-import portfolio from '../../portfolio.json'
-import Card from './Card.js'
+import portfolio from "../../portfolio.json";
+import Card from "./Card.js";
+
+const styles = {
+    grid: {
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr',
+        gap: '2%',
+        textAlign: 'center',
+        marginBottom: '10%'
+    },
+    head: {
+        fontSize: '2em',
+        textAlign: 'center'
+    }
+}
 
 export default function Portfolio() {
-    return (
-        <section>
-            <h1>Projects</h1>
-            <div>
-                {portfolio.map(portfolio => (
-                    <Card key={portfolio.id} image={portfolio.image} title={portfolio.title} github={portfolio.github} link={portfolio.link} />
-                ))}
-            </div>
-        </section>
-    )
+  return (
+    <section>
+      <h1 style={styles.head}>Projects</h1>
+      <div style={styles.grid}>
+        {portfolio.map((portfolio) => (
+          <Card
+            key={portfolio.id}
+            image={portfolio.image}
+            title={portfolio.title}
+            github={portfolio.github}
+            link={portfolio.link}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
